6d783252038555d212d8d05c06f14dfe12646514
Jeffrey Lamar <jlam3950@gmail.com>
2022-05-23 13:26:49 -0400

    added folder for airline logos. updated HTML to include pre-defined values. updated lines 50,51 in async.js

--- public/js/async.js
+++ public/js/async.js
@@ -1,43 +1,43 @@
-
-//load flight data 
-
-// let searchFrom = document.querySelector('#searchFrom').value;
-// let searchTo = document.querySelector('#searchTo').value;
-// let depDate = document.querySelector('#departureDate').value;
-// let arrDate = document.querySelector('#arrivalDate').value;
+let searchFrom = document.querySelector('#searchFrom');
+let searchTo = document.querySelector('#searchTo');
+let depDate = document.querySelector('#departureDate');
+let arrDate = document.querySelector('#arrivalDate');
+let searchForm = document.querySelector('#airportForm');
+let subBtn = document.querySelector('#submitBtn');
+let resetBtn = document.querySelector('#resetBtn');
 let displayFlightDiv = document.querySelector('#flightsContainer');
 
-// searchBtn.addEventListener('click', function(){
-//     loadFlightData();
-// })
+subBtn.addEventListener('click', function(){
+    loadFlightData();
+});
+
+resetBtn.addEventListener('click', function(){
+    searchForm.reset();
+});
 
 const loadFlightData = async() =>{
-   try { const url = "https://tequila-api.kiwi.com/v2/search?fly_from=LGA&fly_to=MIA&dateFrom=01/06/2022&dateTo=02/06/2022"
-   //`https://tequila-api.kiwi.com/v2/search?fly_from=${searchFrom}&fly_to=${searchTo}&dateFrom=${depDate}&dateTo=${arrDate}` <- will use once HTML is functional 
+   let key = await sendKeys();
+
+   try { const url = `https://tequila-api.kiwi.com/v2/search?fly_from=${searchFrom.value}&fly_to=${searchTo.value}&dateFrom=${depDate.value}&dateTo=${arrDate.value}`;
     const response = await fetch( url, {
         method: 'GET', 
         headers: {
-        'apikey': 'zo882tX9KKDMHn9SM5MOE-1Raqm3Pn_E',
+        'apikey': key.tequilaKey,
                 }
     })
-    console.log(response.ok) 
     const data = await response.json();
-    //could trial destructuring. {flyFrom, flyTo, local_arrival, local_departure} = data; 
     console.log(data.data);
-    simplifyData(data.data);
+    limitResults(data.data);
     }catch(error){
         console.log(error);
     }
 };
 
-const simplifyData = (flightData) => {
+const limitResults = (flightData) => {
     let flightArr = [];
-    // const flightData = {flyFrom, flyTo, local_arrival, local_departure};
-
     for (let i = 0; i < 10; i++){
         flightArr[i] = flightData[i]
     }
-    console.log(flightArr);
     renderFlightData(flightArr);
 }
 
@@ -48,13 +48,14 @@ const renderFlightData = (arr) =>{
             <h3>${flight.airlines}<h3>
             <div>From:${flight.flyFrom}</div>
             <div>To:${flight.flyTo}</div>
-            <div>Departs:${flight.local_departure}</div>
-            <div>Arrives:${flight.local_arrival}</div>`
+            <div>Departs:${(flight.local_departure).slice(11,16)}</div>
+            <div>Arrives:${(flight.local_arrival).slice(11,16)}</div>
+            <div>Price:${flight.price}
+            // <div>Duration:${(flight.duration.departure)/60}
+            /<div>`
     })
 }
 
-// loadFlightData();
 
-//userInput for Flights
 
 
